10b821104e33ae69eac6342211f1ef00a61eb2ae
RoJinx <mar.lewandowski@gmail.com>
2022-11-29 14:15:42 +0100

    Added test and implementation

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,9 +1,31 @@
 import React from "react";
-import './Map.css'
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import '../../utils/fix-map-icon.ts';
+
+import 'leaflet/dist/leaflet.css';
+import './Map.css';
+
 export const Map = ()=>{
   return (
     <div className='map'>
-      ...
+      <MapContainer center={[52.5525931,19.703432]} zoom={13}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> & contributors'
+        />
+        <Marker position={[52.5525931,19.703432]}>
+          <Popup>
+            <h2>Coś w Płockowie</h2>
+            <p>I opis do tego</p>
+          </Popup>
+        </Marker>
+        <Marker position={[52.5527606,19.6995239]}>
+          <Popup>
+            <h2>Coś w Płockowie obok</h2>
+            <p>I opis do tego</p>
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   )
 }
